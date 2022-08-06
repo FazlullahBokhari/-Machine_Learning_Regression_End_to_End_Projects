3ce5c2aa5fe2c41347a9e4fa702f20cce969e882
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-06 16:22:27 +0530

    version 0.0.2

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from setuptools import setup 
+from setuptools import setup,find_packages
 from typing import List 
 
 def get_requirements_list()->List[str]:
     " This function will return the list of requirement mention in requirements.txt file "
     with open(REQUIREMENT_FILE_NAME) as requiremet_file:
-        return  requiremet_file.readlines()
+        return  requiremet_file.readlines().remove("-e .")
 
 
 #Declaring varibles for setup functions 
 PROJECT_NAME = "housing-predictor"
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 AUTHOR = "FAZLULLAH BOKHARI"
 
 DESCRIPTION = "This is a first version of housing-predictor" 
 
-PACKAGES = ['housing'] 
+PACKAGES = find_packages()
 
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
