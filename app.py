1bf01d8b66dbe67bd49d14d1e8cbac8633486f80
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-07 08:54:26 +0530

    exception added

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask 
 import numpy as np
 from housing.logger import logging 
-
+from housing.exception import HousingException 
+import sys 
 
 app = Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("We are testing logging module")
+    try:
+        raise Exception("we are testing cutom exception ")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message) 
+        logging.info("We are testing logging module")
     return "Machine Learning Pipeline created CI/CD pipline"
 
 
