7ed5feb7dcb9aa80a568928d9bddf663513691a6
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-10 15:02:14 +0530

    files added

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -1,10 +1,19 @@
 from housing.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig,ModelTrainerConfig,\
     ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
+from util.util import read_yaml_file 
+from housing.constant import * 
+from housing.exception import HousingException 
+import sys 
+
 
 
 class Configuration:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                 config_file_path:str = CONFIG_FILE_PATH,
+                 current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
+        self.config_info = read_yaml_file(file_path=config_file_path) 
+        self.training_pipeline_config = self.get_training_pipeline_config()
+        self.time_stamp = current_time_stamp
     
     def get_data_ingestion_config(self)->DataIngestionConfig:
         pass 
@@ -25,5 +34,8 @@ class Configuration:
         pass 
     
     def get_training_pipeline_config(self)->TrainingPipelineConfig:
-        pass 
+        try:
+            pass
+        except Exception as e: 
+            raise HousingException(e, sys) from e 
     
