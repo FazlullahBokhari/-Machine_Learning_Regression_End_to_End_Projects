197b72436788b4f0d02dcfa62e6bfd4e98a13fd3
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-11 08:15:50 +0530

    constant added

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -23,7 +23,26 @@ class Configuration:
         
     
     def get_data_ingestion_config(self)->DataIngestionConfig:
-        pass 
+        try:
+            self.config_info 
+            
+            dataset_download_url
+            tgz_download_dir
+            raw_data_dir
+            ingested_train_dir
+            ingested_test_dir
+                
+            data_ingestion_config = DataIngestionConfig(
+                dataset_download_url=dataset_download_url,
+                tgz_download_dir=tgz_download_dir,
+                raw_data_dir=raw_data_dir,
+                ingested_train_dir=ingested_train_dir,
+                ingested_test_dir=ingested_test_dir
+            )
+            logging.info(f"Data Ingestion config: {data_ingestion_config}")
+            return data_ingestion_config
+        except Exception as e:
+            raise HousingException(e,sys) from e 
     
     def get_data_validation_config(self)->DataValidationConfig:
         pass 
