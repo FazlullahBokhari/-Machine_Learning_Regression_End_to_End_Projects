5a2bbfe33ef8a000dc70b7c54ea51e283df1cad6
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-11 09:28:03 +0530

    file added

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -23,7 +23,7 @@ DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
 DATA_INGESTION_DOWNLOAD_URL_KEY = "dataset_download_url" 
 DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir" 
 DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir" 
-DATA_INGESTION_DIR_NAME_KEY = "ingested_dir"
+DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir" 
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir" 
 
