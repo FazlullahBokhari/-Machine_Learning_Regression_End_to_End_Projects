25cb0d3dd14e776ff155aa3fe235a331174d815f
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-20 11:09:11 +0530

    file added

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -5,6 +5,9 @@ from housing.logger import logging
 import sys, os 
 import tarfile 
 from six.moves import urllib 
+import pandas as pd 
+import numpy as np 
+
 
 
 
@@ -25,6 +28,9 @@ class DataIngestion:
             #folder location to download file 
             tgz_download_dir = self.data_ingestion_config.tgz_download_dir 
             
+            if os.path.exists(tgz_download_dir):
+                os.remove(tgz_download_dir)
+            
             os.makedirs(tgz_download_dir,exist_ok=True) 
             
             housing_file_name = os.path.basename(download_url) 
@@ -42,21 +48,52 @@ class DataIngestion:
         except Exception as e: 
             raise HousingException(e, sys) from e 
     
-    def extract_tgz_file(self):
+    def extract_tgz_file(self, tgz_file_path:str):
         try:
-            pass 
+            raw_data_dir = self.data_ingestion_config.raw_data_dir 
+            
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)  
+                
+            os.makedirs(raw_data_dir,exist_ok=True) 
+            
+            logging.info(f"Extracting tgz file: [{tgz_file_path}] into dir: [{raw_data_dir}]")
+            
+            with tarfile.open(tgz_file_path) as housing_tgz_file_obj:
+                housing_tgz_file_obj.extractall(path=raw_data_dir) 
+                
+            logging.info(f"Extraction completed")
+            
         except Exception as e: 
             raise HousingException(e, sys) from e 
     
-    def split_data_as_train_test(self):
+    def split_data_as_train_test(self) -> DataIngestionArtifact:
         try:
-            pass 
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+            
+            file_name = os.listdir(raw_data_dir)[0] 
+            
+            housing_file_path = os.path.join(raw_data_dir,file_name) 
+            
+            housing_data_frame = pd.read_csv(housing_file_path) 
+            
+            housing_data_frame['income_cat'] = pd.cut(
+                housing_data_frame['median_income'],
+                bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+                labels=[1,2,3,4,5]
+            )
+            
+            strat_train_set = None 
+            strat_test_set = None 
+            
         except Exception as e: 
             raise HousingException(e, sys) from e 
     
     def initiate_data_ingestion(self)->DataIngestionArtifact: 
         try:
             tgz_file_path = self.download_housing_data() 
+            
+            self.extract_tgz_file(tgz_file_path=tgz_file_path)
         except Exception as e:
             raise HousingException(e, sys) from e 
     
