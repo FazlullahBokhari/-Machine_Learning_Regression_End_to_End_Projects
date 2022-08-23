bc396ebf22d569b3dbbbb981824f5e95826db21a
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-23 10:32:49 +0530

    file added

--- housing/component/data_validation.py
+++ housing/component/data_validation.py
@@ -15,20 +15,51 @@ class DataValidation:
         except Exception as e:
             raise HousingException(e,sys) from e 
         
-    def is_train_test_file_exists():
+    def is_train_test_file_exists(self) -> bool:
         try:
             is_train_file_exist = False 
             is_test_file_exist = False 
             
-            train_file_path = self.data_ingestion_artifact.is_train_file_exist 
-            test_file_path = self.data_ingestion_artifact.is_test_file_exist 
+            logging.info("Checking if training and test file is available ")
             
-            return is_train_file_exist and is_test_file_exist 
+            train_file_path = self.data_ingestion_artifact.train_file_exist 
+            test_file_path = self.data_ingestion_artifact.test_file_exist  
+            
+            is_train_file_exist = os.path.exists(train_file_path) 
+            is_test_file_exist = os.path.exists(test_file_path)  
+            
+            
+            is_available =  is_train_file_exist and is_test_file_exist 
+            
+            logging.info(f"Is train and test file exists? {is_available}") 
+            
+            if not is_available:
+                training_file = self.data_ingestion_artifact.train_file_path 
+                testing_file = self.data_ingestion_artifact.test_file_path
+                
+                message = f"Training file: {training_file} or Testing file: {testing_file} not present "
+                logging.info(message)
+                raise Exception(message)
+            
+            return is_available 
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise HousingException(e,sys) from e 
+        
+    def validate_dataset_schema(self)-> bool:
+        try:
+            validation_status = False 
+            
+            
+            validation_status = True 
+            
+            return validation_status
+        except Exception as e:
+            raise HousingException(e, sys) from e
         
     def initiate_data_validation(self):
         try:
-            pass 
+            self.is_train_test_file_exists() 
+            self.validate_dataset_schema() 
+            
         except Exception as e:
             raise HousingException(e,sys) from e
