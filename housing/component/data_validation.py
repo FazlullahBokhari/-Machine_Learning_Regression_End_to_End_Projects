5bf6b9fd65ba1c78479a6f2835abd8f1b747c827
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-24 10:08:37 +0530

    file added

--- housing/component/data_validation.py
+++ housing/component/data_validation.py
@@ -3,6 +3,13 @@ from housing.exception import HousingException
 from housing.entity.config_entity import DataValidationConfig 
 from housing.entity.artifact_entity import DataIngestionArtifact 
 import os, sys 
+import pandas as pd
+from evidently.model_profile import Profile 
+from evidently.model_profile.sections import DataDriftProfileSection 
+from evidently.dashboard import Dashboard 
+from evidently.dashboard.tabs import DataDriftTab 
+import json 
+
 
 
 class DataValidation:
@@ -13,7 +20,16 @@ class DataValidation:
             self.data_validation_config = data_validation_config
             self.data_ingestion_artifact = data_ingestion_artifact
         except Exception as e:
-            raise HousingException(e,sys) from e 
+            raise HousingException(e,sys) from e  
+        
+    def get_train_and_test_df(self):
+        try:
+            train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path) 
+            
+            return train_df, test_df 
+        except Exception as e:
+            raise HousingException(e, sys) from e 
         
     def is_train_test_file_exists(self) -> bool:
         try:
@@ -56,10 +72,52 @@ class DataValidation:
         except Exception as e:
             raise HousingException(e, sys) from e
         
+    def get_and_save_data_drift_report(self):
+        try:
+            profile = Profile(sections=[DataDriftProfileSection()]) 
+            train_df, test_df = self.get_train_and_test_df() 
+            
+            profile.calculate(train_df,test_df) 
+        
+            
+            report = json.loads(profile.json()) 
+            
+            with open(self.data_validation_config.report_file_path,'w') as report_file:
+                json.dump(report, report_file, indent=6) 
+                
+            return report
+             
+        except Exception as e:
+            raise HousingException(e,sys) from e
+        
+    def save_data_drift_report_page(self):
+        try:
+            dashboard = Dashboard(tabs=[DataDriftTab()]) 
+            
+            train_df, test_df = self.get_train_and_test_df() 
+            
+            dashboard.calculate(train_df,test_df) 
+            
+            dashboard.save(self.data_validation_config.report_page_file_path) 
+            
+        except Exception as e:
+            raise HousingException(e,sys) from e
+    
+    def is_data_drift_found(self)->bool:
+        try:
+            report = self.get_and_save_data_drift_report()
+            self.save_data_drift_report_page() 
+            
+             
+            return True
+        except Exception as e:
+            raise HousingException(e,sys) from e
+    
+        
     def initiate_data_validation(self):
         try:
             self.is_train_test_file_exists() 
             self.validate_dataset_schema() 
-            
+            self.is_data_drift_found() 
         except Exception as e:
             raise HousingException(e,sys) from e
