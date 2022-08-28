9f4aaef9e16c7752ea166803f51a5595df918c96
fazlullahb0 <fazlullahb0@gmail.com>
2022-08-28 13:01:20 +0530

    fole added

--- demo.py
+++ demo.py
@@ -6,10 +6,12 @@ import sys
 
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline() 
+        #pipeline = Pipeline()
+        #pipeline.run_pipeline() 
         #data_validation_config = Configuration().get_data_validation_config()
-        #print(data_validation_config)
+        #print(data_validation_config) 
+        data_transformation_config = Configuration().get_data_transformation_config() 
+        print(data_transformation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
